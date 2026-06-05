0c3f4a65d8a88e5142a87a9a74a3b9f348ed3a34
corn-snake <snakecorn@proton.me>
2026-06-05 19:15:22 +0100

    HOTFIX: accidentally leaked in next changeset and broke everything

--- src/components/atoms/HistoryLine.vue
+++ src/components/atoms/HistoryLine.vue
@@ -1,12 +1,10 @@
 <script setup>
     import { compBop, bopData } from '../../stores/bopstore';
     import GenericLine from './GenericLine.vue';
-    import NewTurn from './NewTurn.vue';
     const props = defineProps(["st"]);
     defineEmits(["selTurn"]);
 </script>
 <template>
     <GenericLine :array="bopData.claim < 1 ? compBop.history.map(h=>h.number) : compBop.history" :si="props.st" @selItem="n=>$emit('selTurn', n.value)" pronomen="T">
-        <NewTurn v-if="bopData.claim === 2 && bopData.turn === compBop.history.at(-1)"/>
     </GenericLine>
 </template>
